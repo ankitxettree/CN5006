8acc2d3e72138f516756f1bf3e76433af66ceb44
Ankit Chhetri Thapa <91187491+ankitxettree@users.noreply.github.com>
2025-10-17 11:54:52 +0000

    updated

--- Week1/index.js
+++ Week1/index.js
@@ -11,13 +11,14 @@ console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
 const prompt = require('prompt-sync')(); // This line is essential 
 console.log("starting") 
 const name = prompt('Enter your name: '); 
-console.log(Hello, ${name}`);
-//program that checks if the number is positive, negative or zero
-// input from the user
+console.log(Hello, ${name});
 const number = parseInt(prompt("Enter a number: "));
-// check if number is greater than 
 if (number > 0) { 
-console.log("The number is positive"); }
-check if number is else if (number == 0){ } 
-// console.log("The number is zero"); // if number is less than else { 
-// console.log("The number is negative");
+console.log("The number is positive");
+}
+else if (number == 0){ 
+console.log("The number is zero"); 
+}
+else{ 
+    console.log("The number is negative");
+}
