bcb431bd72c10025f814f9a3e9207ed9fc921f62
Ankit Chhetri Thapa <91187491+ankitxettree@users.noreply.github.com>
2025-10-17 11:55:38 +0000

    up

--- Week1/index.js
+++ Week1/index.js
@@ -1,11 +1,13 @@
 //exercise1
 console.log(“This is my first programe ”)
 console.log(“Well come John your month salary is 500000”)
-//exercise
+
+//exercise2
 const num1 = 5;
 const num2 = 3; // add two numbers
 const sum = num1 + num2; // display the sum 
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
 //exercise3
 //app.js or index.js
 const prompt = require('prompt-sync')(); // This line is essential 
